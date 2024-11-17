d7d02cdba0d7e06a8ff58212a05fd7e77a5cf54e
miaoice <miaouwu@foxmail.com>
2024-11-17 14:56:55 +0800

    Update ChatCommand.cs

--- src/Other_code/key/ChatCommand.cs
+++ src/Other_code/key/ChatCommand.cs
@@ -8,34 +8,42 @@ namespace MH;
 [HarmonyPatch(typeof(ChatController), nameof(ChatController.SendFreeChat))]
 public static class ChatController_SendFreeChat
 {
-    public static string Command = "";
+    private static string Text;
     public static bool Prefix(ChatController __instance)
     {
-        string text = __instance.freeChatField.Text;
-        if(text == "/music")
-            if(C.Music)
-            {
-                C.Music = false;
-                S.Com("已开启音乐");
-            }
-            else
-            {
-                C.Music = true;
-                S.Com("已关闭音乐");
-            }
+        Text = __instance.freeChatField.Text;
+        if(Text == "/music")
+        {
+            Music();
+            return false;
+        }
 
 
         //改名
         if(C.Rname == true)
         {
-            S.RN(text); 
-            C.Rname = false;
-            S.Com("<color=#951716>已改名</color>");
+            Rname();
         }
         //发消息
         else
-        S.Chat(text); // 发送原始消息
+        S.Chat(Text); // 发送原始消息
 
         return false; // 阻止原方法执行
     }
+    private static void Music()
+    {
+        if(C.Music)
+        {
+            C.Music = !C.Music;   S.Com("已开启音乐");
+        }else{
+            C.Music = true;   S.Com("已关闭音乐.");
+        }
+    }
+    private static void Rname()
+    {
+            S.RN(Text); 
+            C.Rname = false;
+            S.Com("<color=#951716>已改名</color>");
+    }
+    
 }
